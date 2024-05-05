afcaccde879679556277434af522322bd16057b9
milanzala <tinyapps1@gmail.com>
2024-05-05 17:11:19 +0530

    updated dependencies

--- js/build.gradle.kts
+++ js/build.gradle.kts
@@ -2,14 +2,15 @@ plugins {
   //  alias(libs.plugins.androidLibrary)
  //   alias(libs.plugins.jetbrainsKotlinAndroid)
     id("com.android.library")
-    id("kotlin-android")
+    id("org.jetbrains.kotlin.android")
     id("maven-publish")
 
 }
 
 
 group = "com.github.tinyCodes1"
-version = "1.2"
+version = "1.3"
+
 
 
 android {
@@ -17,12 +18,13 @@ android {
     compileSdk = 34
 
     defaultConfig {
-        aarMetadata {
+     /*   aarMetadata {
             minCompileSdk = 26
-        }
+        }*/
         minSdk = 26
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
+        vectorDrawables { useSupportLibrary = true }
     }
 
     buildTypes {
@@ -41,23 +43,47 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.1"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
 }
+
+
 
 dependencies {
 
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+        implementation("androidx.compose.ui:ui-graphics:1.6.7")
+        implementation("androidx.documentfile:documentfile:1.0.1")
+        implementation("androidx.compose.ui:ui:1.6.7")
+        debugImplementation("androidx.compose.ui:ui-tooling:1.6.7")
+        implementation("androidx.compose.ui:ui-tooling-preview:1.6.7")
+        implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
 
-    implementation(libs.androidx.documentfile)
-    implementation(libs.gson)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    implementation(libs.androidx.activity)
+        implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.9.0")
+
+        implementation("androidx.activity:activity-compose:1.9.0")
+        implementation("androidx.compose:compose-bom:2024.05.00")
+        androidTestImplementation("androidx.compose:compose-bom:2024.05.00")
+        implementation("androidx.compose.material3:material3:1.2.1")
+        androidTestImplementation("androidx.compose.ui:ui-test-junit4-desktop:1.6.7")
+        debugImplementation("androidx.compose.ui:ui-test-manifest:1.6.7")
+
+        implementation("androidx.core:core-ktx:1.13.1")
+        testImplementation("androidx.test.ext:junit:1.1.5")
+        androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.6.7")
+        androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+
 }
+
 
 
 afterEvaluate {
@@ -67,9 +93,10 @@ afterEvaluate {
             create<MavenPublication>("release") {
                 from(components["release"])
                 groupId = "com.github.tinyCodes1"
-                artifactId = "addjs"
-                version = "1.2"
+                artifactId = "js"
+                version = "1.3"
             }
         }
     }
 }
+
