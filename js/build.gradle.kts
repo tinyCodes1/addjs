86f6ca8db42430dec4f3278d904bfe15745603c0
milanzala <tinyapps1@gmail.com>
2024-05-05 17:26:07 +0530

    changed evaluation

--- js/build.gradle.kts
+++ js/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-  //  alias(libs.plugins.androidLibrary)
- //   alias(libs.plugins.jetbrainsKotlinAndroid)
+    //  alias(libs.plugins.androidLibrary)
+    //   alias(libs.plugins.jetbrainsKotlinAndroid)
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
     id("maven-publish")
@@ -18,9 +18,9 @@ android {
     compileSdk = 34
 
     defaultConfig {
-     /*   aarMetadata {
-            minCompileSdk = 26
-        }*/
+        /*   aarMetadata {
+               minCompileSdk = 26
+           }*/
         minSdk = 26
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
@@ -31,8 +31,7 @@ android {
         release {
             isMinifyEnabled = false
             proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
+                getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro"
             )
         }
     }
@@ -60,43 +59,42 @@ android {
 
 dependencies {
 
-        implementation("androidx.compose.ui:ui-graphics:1.6.7")
-        implementation("androidx.documentfile:documentfile:1.0.1")
-        implementation("androidx.compose.ui:ui:1.6.7")
-        debugImplementation("androidx.compose.ui:ui-tooling:1.6.7")
-        implementation("androidx.compose.ui:ui-tooling-preview:1.6.7")
-        implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+    implementation("androidx.compose.ui:ui-graphics:1.6.7")
+    implementation("androidx.documentfile:documentfile:1.0.1")
+    implementation("androidx.compose.ui:ui:1.6.7")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.6.7")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.6.7")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
 
-        implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.9.0")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.9.0")
 
-        implementation("androidx.activity:activity-compose:1.9.0")
-        implementation("androidx.compose:compose-bom:2024.05.00")
-        androidTestImplementation("androidx.compose:compose-bom:2024.05.00")
-        implementation("androidx.compose.material3:material3:1.2.1")
-        androidTestImplementation("androidx.compose.ui:ui-test-junit4-desktop:1.6.7")
-        debugImplementation("androidx.compose.ui:ui-test-manifest:1.6.7")
+    implementation("androidx.activity:activity-compose:1.9.0")
+    implementation("androidx.compose:compose-bom:2024.05.00")
+    androidTestImplementation("androidx.compose:compose-bom:2024.05.00")
+    implementation("androidx.compose.material3:material3:1.2.1")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4-desktop:1.6.7")
+    debugImplementation("androidx.compose.ui:ui-test-manifest:1.6.7")
 
-        implementation("androidx.core:core-ktx:1.13.1")
-        testImplementation("androidx.test.ext:junit:1.1.5")
-        androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.6.7")
-        androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation("androidx.core:core-ktx:1.13.1")
+    testImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.6.7")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
 
 }
 
 
 
-afterEvaluate {
-    publishing {
-        publications {
-            // Creates a Maven publication called "release".
-            create<MavenPublication>("release") {
-                from(components["release"])
-                groupId = "com.github.tinyCodes1"
-                artifactId = "addjs"
-                version = "1.3"
-            }
+publishing {
+    publications {
+        // Creates a Maven publication called "release".
+        create<MavenPublication>("release") {
+            from(components["release"])
+            groupId = "com.github.tinyCodes1"
+            artifactId = "addjs"
+            version = "1.3"
         }
     }
 }
+
 
