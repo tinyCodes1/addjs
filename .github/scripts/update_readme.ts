39b54656f3e715703a3865e233469404aca5e54b
milanzala <tinyapps1@gmail.com>
2024-05-07 16:18:21 +0530

    changed for debug

--- .github/scripts/update_readme.ts
+++ .github/scripts/update_readme.ts
@@ -1,12 +1,8 @@
 const ref : string | undefined = Deno.env.get('GITHUB_REF');
 
 if (ref) {
- const version = ref.replace("/refs/tags/", "")
+ const version = ref.replace("refs/tags/", "")
 console.log(`output is: ${version} ... type: ${typeof version}`);
-console.log(`version: ` + version.replace("/refs/tags/", ""))
-
-console.log(`current directory is : ${Deno.cwd()}`);
-
 
 let readmeContent: string = Deno.readTextFileSync('readme.md');
 
