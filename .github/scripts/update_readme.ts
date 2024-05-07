55f162cb771260b1473677e1044a8b1c362c9f8a
milanzala <tinyapps1@gmail.com>
2024-05-07 16:40:16 +0530

    changed for debug

--- .github/scripts/update_readme.ts
+++ .github/scripts/update_readme.ts
@@ -7,6 +7,7 @@ if (ref) {
 
     let readmeContent: string = Deno.readTextFileSync('readme.md');
 console.log(readmeContent);
-    readmeContent = readmeContent.replace(/:addjs:v.*\..*\"/g, `:addjs:${version}`) ;
+    readmeContent = readmeContent.replace(/simple/g, `complex`) ;
+//    readmeContent = readmeContent.replace(/:addjs:v.*\..*\"/g, `:addjs:${version}`) ;
     Deno.writeTextFileSync('readme.md', readmeContent);
 }
