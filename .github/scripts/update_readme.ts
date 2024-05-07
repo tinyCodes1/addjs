9e6e9739d3a8125d7aff49c13676c1a08bcaaaf9
milanzala <tinyapps1@gmail.com>
2024-05-07 15:04:50 +0530

    changed for debug

--- .github/scripts/update_readme.ts
+++ .github/scripts/update_readme.ts
@@ -1,24 +1,16 @@
-
-const version: string | undefined = Deno.env.get('VERSION');
-
 const output: string | undefined = Deno.env.get('OUTPUT');
 
-console.log(`version is: ${version}`);
 console.log(`output is: ${output} ... type: ${typeof output}`);
 
 if (output) {
-
+ const version = output.replace("/refs/tags/", "")
 console.log(`version: ` + output.replace("/refs/tags/", ""))
-}
 
 console.log(`current directory is : ${Deno.cwd()}`);
 
-if (!version) {
-    throw new Error('VERSION environment variable not found.');
-}
 
 let readmeContent: string = Deno.readTextFileSync('readme.md');
 
 readmeContent = readmeContent.replace(/:addjs:v\[\d\]+\.\[\d\]+/, `:addjs:${version}`) ;
 Deno.writeTextFileSync('readme.md', readmeContent);
-
+}
